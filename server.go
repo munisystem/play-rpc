f32cf3f09f733fe1fbfb44a24437711f676a5a97
Yuichi Saito <munisystem@gmail.com>
2017-03-02 15:26:06 +0900

    Add rpc server

--- server.go
+++ server.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"net/rpc"
+)
+
+type Arith int
+
+func (a *Arith) Fib(n int, reply *int) {
+	*reply = FibTail(n)
+}
+
 func FibTail(n int, a ...int) int {
 	if len(a) == 0 {
 		a = []int{1, 0}
@@ -14,4 +26,15 @@ func FibTail(n int, a ...int) int {
 	}
 
 	return FibTail(n-1, a[0]+a[1], a[0])
+}
+
+func main() {
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+
+	err := http.ListenAndServe(":6262", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
